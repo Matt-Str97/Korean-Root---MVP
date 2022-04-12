ebb055fc89b0940c068dc53fd8247ba1a7fad22b
NTabbita <nico.tabbita@gmail.com>
2022-04-12 15:52:03 -0300

    creacion de clase y settings para el blog

--- AppBlog/models.py
+++ AppBlog/models.py
@@ -1,3 +1,13 @@
+from email.policy import default
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    titulo = models.CharField(max_length=70)
+    subtitulo = models.CharField(max_length=140)
+    cuerpo = models.TextField()
+    fecha_creacion = models.DateTimeField()
+    autor = models.CharField(max_length=50)
+    img_portada = models.ImageField(upload_to='portadas', height_field=None, width_field=None, default='portadadefault.jpg')
+    img_miniatura = models.ImageField(upload_to='miniaturas', height_field=None, width_field=None, default='miniaturadefault.jpg')
+
